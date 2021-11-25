ba07f3540716d896acca4564e9cf4292df4d3d6e
AbsurdMantis <raphaelbholmes@gmail.com>
2021-11-25 18:00:22 -0300

    Classes de Status

---
+++ Pokedex/Models/Stat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Models
+{
+    public class Stat
+    {
+        public long base_stat { get; set; }
+        public long effort { get; set; }
+        public Species stat { get; set; }
+    }
+}
